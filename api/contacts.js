b0a9592373ef9f5d85b58b30aae2556620c6be35
Mariia <mcherkashina77@gmail.com>
2025-02-27 23:14:14 +0100

    Update contacts.js

--- api/contacts.js
+++ api/contacts.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const contactsFilePath = path.join(__dirname, '../models/contacts.json');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(cors()); 
 app.use(express.json());
